646414db4eefd015e09872d4582ddf676435b4d8
Bonnie <kbonnie@gmail.com>
2017-02-07 20:48:29 -0800

    Working on build room, have random numbers

--- buildrooms.c
+++ buildrooms.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <time.h>
+#include <stdlib.h>
 
 
 // Prototypes
@@ -29,12 +31,33 @@ void createDirectory(){
 }
 
 void createRooms(){
-  printf("creating room\n");
+
   printf("The first room name is %s\n", roomNames[0]);
+
+  // create a file
+  FILE *fp = NULL;
+  //  a is for append, w is for write
+  fp = fopen("./foo/textFile.txt" ,"w");
+  fprintf(fp, "writing in this file");
+
+  // close
+  fclose(fp);
+
+
+
 
   /* hardcode 10 room names
   // but randomly only create 7 rooms
   one is start, one is end, and others are middle
 
+  To pick 7 rooms, pick number between 1-10
+  check if exists in array, if so add to room array
   */
+
+
+  // pick random number between one and 10 with different seed
+  int r;
+  srand(time(NULL));
+  r = rand()%10;
+  printf("The random number is %d\n",r );
 }
